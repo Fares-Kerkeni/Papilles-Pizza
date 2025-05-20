bed4a55d781b1ac35a9ca5e84d26b6b862cc9642
Fares Kerkeni <93195495+Fares-Kerkeni@users.noreply.github.com>
2025-05-20 20:50:09 +0200

    all men

--- project/src/components/Footer.tsx
+++ project/src/components/Footer.tsx
@@ -1,31 +1,30 @@
-import { Phone, MapPin, Clock, Pizza } from 'lucide-react';
+import { Phone, MapPin, Clock } from "lucide-react";
+import logo from "/logo.png";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
-  
+
   return (
     <footer className="bg-white/80 pt-12 pb-6 border-t border-gray-200">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-8">
           <div>
             <div className="flex items-center mb-4">
-              <Pizza className="h-8 w-8 text-yellow-500 mr-2" />
-              <span className="font-bold text-xl bg-gradient-to-r from-red-600 to-yellow-500 bg-clip-text text-transparent">
-                Papilles Pizza
-              </span>
+              <img className="h-11 min-w-[100px]" src={logo} alt="Logo" />
             </div>
             <p className="text-gray-600 mb-4">
-              Des pizzas délicieuses préparées avec des ingrédients frais et de qualité.
+              Des pizzas délicieuses préparées avec des ingrédients frais et de
+              qualité.
             </p>
             <div className="flex space-x-4">
-              <a 
-                href="tel:0169806552" 
+              <a
+                href="tel:0169806552"
                 className="text-gray-600 hover:text-primary transition-colors"
                 aria-label="Téléphone"
               >
                 <Phone className="h-6 w-6" />
               </a>
-              <a 
+              <a
                 href="https://www.google.com/maps?sca_esv=44cd9e4f38d377fc&sxsrf=AHTn8zodi5lgB5JhR4UOhPRxbgs9QnoyoQ:1747594816168&uact=5&gs_lp=Egxnd3Mtd2l6LXNlcnAiHXBhcGlsbGVzIHBpenphIG1hcmNvdXNzaXMgbWFwMgIQJjIIEAAYogQYiQUyCBAAGIAEGKIEMggQABiABBiiBDIIEAAYgAQYogRIvxdQrhNYrRVwA3gBkAEAmAFBoAHBAaoBATO4AQPIAQD4AQGYAgagAt8BwgIKEAAYsAMY1gQYR8ICChAjGIAEGCcYigWYAwCIBgGQBgiSBwE2oAedCrIHATO4B80B&um=1&ie=UTF-8&fb=1&gl=fr&sa=X&geocode=Kc8jV0Fm1-VHMR7xCuTLOhZp&daddr=23+Rue+Alfred+Dubois,+91460+Marcoussis"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -36,9 +35,11 @@ const Footer = () => {
               </a>
             </div>
           </div>
-          
+
           <div>
-            <h3 className="text-lg font-bold mb-4 text-gray-900">Horaires d'ouverture</h3>
+            <h3 className="text-lg font-bold mb-4 text-gray-900">
+              Horaires d'ouverture
+            </h3>
             <ul className="space-y-2 text-gray-600">
               <li className="flex justify-between">
                 <span>Lundi à Samedi</span>
@@ -50,25 +51,28 @@ const Footer = () => {
               </li>
               <li className="flex justify-between">
                 <span>Dimanche</span>
-                <span>Soir uniquement</span>
+                <span>18h - 23h30</span>
               </li>
             </ul>
           </div>
-          
+
           <div>
             <h3 className="text-lg font-bold mb-4 text-gray-900">Contact</h3>
             <ul className="space-y-2 text-gray-600">
               <li>
-                <a href="tel:0169806552" className="flex items-start hover:text-primary transition-colors">
+                <a
+                  href="tel:0169806552"
+                  className="flex items-start hover:text-primary transition-colors"
+                >
                   <Phone className="h-5 w-5 mr-2 mt-1 flex-shrink-0" />
                   <span>01 69 80 65 52</span>
                 </a>
               </li>
               <li>
-                <a 
+                <a
                   href="https://www.google.com/maps?sca_esv=44cd9e4f38d377fc&sxsrf=AHTn8zodi5lgB5JhR4UOhPRxbgs9QnoyoQ:1747594816168&uact=5&gs_lp=Egxnd3Mtd2l6LXNlcnAiHXBhcGlsbGVzIHBpenphIG1hcmNvdXNzaXMgbWFwMgIQJjIIEAAYogQYiQUyCBAAGIAEGKIEMggQABiABBiiBDIIEAAYgAQYogRIvxdQrhNYrRVwA3gBkAEAmAFBoAHBAaoBATO4AQPIAQD4AQGYAgagAt8BwgIKEAAYsAMY1gQYR8ICChAjGIAEGCcYigWYAwCIBgGQBgiSBwE2oAedCrIHATO4B80B&um=1&ie=UTF-8&fb=1&gl=fr&sa=X&geocode=Kc8jV0Fm1-VHMR7xCuTLOhZp&daddr=23+Rue+Alfred+Dubois,+91460+Marcoussis"
                   target="_blank"
-                  rel="noopener noreferrer" 
+                  rel="noopener noreferrer"
                   className="flex items-start hover:text-primary transition-colors"
                 >
                   <MapPin className="h-5 w-5 mr-2 mt-1 flex-shrink-0" />
@@ -82,7 +86,7 @@ const Footer = () => {
             </ul>
           </div>
         </div>
-        
+
         <div className="border-t border-gray-200 pt-6 text-center text-gray-500">
           <p>© {currentYear} Papilles Pizza - Tous droits réservés</p>
         </div>
--- project/src/data/menuData.ts
+++ project/src/data/menuData.ts
@@ -1,294 +1,544 @@
-import { MenuCategory } from '../types';
+import { MenuCategory } from "../types";
 
-export const menuData: Record<string, MenuCategory['items']> = {
-  "pizzas": [
+export const menuData: Record<string, MenuCategory["items"]> = {
+  pizzas: [
     {
-      "name": "Classica",
-      "description": "Tomate, fromage",
-      "options": [
-        {
-          "name": "Petite",
-          "price": "8,00 €"
-        },
-        {
-          "name": "Moyenne",
-          "price": "12,00 €"
-        },
-        {
-          "name": "Grande",
-          "price": "16,00 €"
-        }
-      ]
+      name: "CLASSICA",
+      description: "Tomate, fromage",
+      options: [
+        { name: "Petite", price: "8,00 €" },
+        { name: "Moyenne", price: "12,00 €" },
+        { name: "Grande", price: "16,00 €" },
+      ],
     },
     {
-      "name": "Calzone",
-      "description": "Tomate, fromage, jambon, œuf",
-      "options": [
-        {
-          "name": "Petite",
-          "price": "9,50 €"
-        },
-        {
-          "name": "Moyenne",
-          "price": "16,00 €"
-        },
-        {
-          "name": "Grande",
-          "price": "21,00 €"
-        }
-      ]
-    }
+      name: "CALZONE",
+      description: "Tomate, fromage, jambon, œuf",
+      options: [
+        { name: "Petite", price: "9,50 €" },
+        { name: "Moyenne", price: "16,00 €" },
+        { name: "Grande", price: "21,00 €" },
+      ],
+    },
+    {
+      name: "NAPOLITAINE",
+      description: "Tomate, fromage, anchois, câpres, olives",
+      options: [
+        { name: "Petite", price: "9,50 €" },
+        { name: "Moyenne", price: "16,00 €" },
+        { name: "Grande", price: "21,00 €" },
+      ],
+    },
+    {
+      name: "REINE",
+      description: "Tomate, fromage, jambon, champignons",
+      options: [
+        { name: "Petite", price: "9,50 €" },
+        { name: "Moyenne", price: "16,00 €" },
+        { name: "Grande", price: "21,00 €" },
+      ],
+    },
+    {
+      name: "CAMPIONE",
+      description: "Tomate, fromage, viande hachée, champignons",
+      options: [
+        { name: "Petite", price: "9,50 €" },
+        { name: "Moyenne", price: "16,00 €" },
+        { name: "Grande", price: "21,00 €" },
+      ],
+    },
+    {
+      name: "NEPTUNE",
+      description: "Tomate, fromage, thon, poivrons, olives vertes",
+      options: [
+        { name: "Petite", price: "9,50 €" },
+        { name: "Moyenne", price: "16,00 €" },
+        { name: "Grande", price: "21,00 €" },
+      ],
+    },
+    {
+      name: "ORIENTALE",
+      description: "Tomate, fromage, merguez, poivrons, oignons, œuf",
+      options: [
+        { name: "Petite", price: "9,50 €" },
+        { name: "Moyenne", price: "16,00 €" },
+        { name: "Grande", price: "21,00 €" },
+      ],
+    },
+    {
+      name: "4 FROMAGES",
+      description: "Tomate, fromage, assortiment de 4 fromages",
+      options: [
+        { name: "Petite", price: "9,50 €" },
+        { name: "Moyenne", price: "16,00 €" },
+        { name: "Grande", price: "21,00 €" },
+      ],
+    },
+    {
+      name: "4 SAISONS",
+      description: "Tomate, fromage, jambon, champignons, poivrons",
+      options: [
+        { name: "Petite", price: "9,50 €" },
+        { name: "Moyenne", price: "16,00 €" },
+        { name: "Grande", price: "21,00 €" },
+      ],
+    },
+    {
+      name: "3 JAMBONS",
+      description: "Tomate, fromage, 3 jambons",
+      options: [
+        { name: "Petite", price: "9,50 €" },
+        { name: "Moyenne", price: "16,00 €" },
+        { name: "Grande", price: "21,00 €" },
+      ],
+    },
+    {
+      name: "VEGETARIENNE",
+      description: "Tomate, fromage, champignons, poivrons, oignons",
+      options: [
+        { name: "Petite", price: "9,50 €" },
+        { name: "Moyenne", price: "16,00 €" },
+        { name: "Grande", price: "21,00 €" },
+      ],
+    },
+    {
+      name: "MEXICAINE",
+      description: "Tomate, fromage, viande hachée, œuf",
+      options: [
+        { name: "Petite", price: "9,50 €" },
+        { name: "Moyenne", price: "16,00 €" },
+        { name: "Grande", price: "21,00 €" },
+      ],
+    },
+    {
+      name: "RACLETTE",
+      description: "Tomate, fromage, jambon, fromage à raclette, œuf",
+      options: [
+        { name: "Petite", price: "9,50 €" },
+        { name: "Moyenne", price: "16,00 €" },
+        { name: "Grande", price: "21,00 €" },
+      ],
+    },
+    {
+      name: "BOLOGNAISE",
+      description: "Tomate, fromage, viande hachée, tomates, oignons",
+      options: [
+        { name: "Petite", price: "9,50 €" },
+        { name: "Moyenne", price: "16,00 €" },
+        { name: "Grande", price: "21,00 €" },
+      ],
+    },
+    {
+      name: "SAN PIETRO",
+      description: "Tomate, fromage, jambon, chorizo",
+      options: [
+        { name: "Petite", price: "9,50 €" },
+        { name: "Moyenne", price: "16,00 €" },
+        { name: "Grande", price: "21,00 €" },
+      ],
+    },
+    {
+      name: "INDIENNE",
+      description:
+        "Sauce curry, fromage, poulet à l'indienne, poivrons, oignons",
+      options: [
+        { name: "Petite", price: "9,50 €" },
+        { name: "Moyenne", price: "16,00 €" },
+        { name: "Grande", price: "21,00 €" },
+      ],
+    },
+    {
+      name: "CANNIBALE",
+      description: "Tomate, fromage, viande hachée, poulet, merguez",
+      options: [
+        { name: "Petite", price: "9,50 €" },
+        { name: "Moyenne", price: "16,00 €" },
+        { name: "Grande", price: "21,00 €" },
+      ],
+    },
+    {
+      name: "ALGERIENNE",
+      description:
+        "Sauce algérienne, fromage, poulet, viande hachée, poivrons, oignons",
+      options: [
+        { name: "Petite", price: "9,50 €" },
+        { name: "Moyenne", price: "16,00 €" },
+        { name: "Grande", price: "21,00 €" },
+      ],
+    },
+    {
+      name: "BARBECUE",
+      description:
+        "Sauce barbecue, fromage, viande hachée, tomates fraîches, oignons, cheddar",
+      options: [
+        { name: "Petite", price: "9,50 €" },
+        { name: "Moyenne", price: "16,00 €" },
+        { name: "Grande", price: "21,00 €" },
+      ],
+    },
+    {
+      name: "BURGER",
+      description:
+        "Tomate, fromage, viande hachée, tomates fraîches, oignons, cheddar",
+      options: [
+        { name: "Petite", price: "9,50 €" },
+        { name: "Moyenne", price: "16,00 €" },
+        { name: "Grande", price: "21,00 €" },
+      ],
+    },
+    {
+      name: "SPICY",
+      description: "Tomate, fromage, viande hachée, chorizo, merguez",
+      options: [
+        { name: "Petite", price: "9,50 €" },
+        { name: "Moyenne", price: "16,00 €" },
+        { name: "Grande", price: "21,00 €" },
+      ],
+    },
+    {
+      name: "CILENTO",
+      description: "Crème fraîche, fromage, poulet, champignons, chèvre",
+      options: [
+        { name: "Petite", price: "9,50 €" },
+        { name: "Moyenne", price: "16,00 €" },
+        { name: "Grande", price: "21,00 €" },
+      ],
+    },
+    {
+      name: "RIMINI",
+      description: "Crème fraîche, fromage, jambon, pommes de terre, œuf",
+      options: [
+        { name: "Petite", price: "9,50 €" },
+        { name: "Moyenne", price: "16,00 €" },
+        { name: "Grande", price: "21,00 €" },
+      ],
+    },
+    {
+      name: "VENEZIA",
+      description: "Crème fraîche, fromage, saumon fumé, citron",
+      options: [
+        { name: "Petite", price: "9,50 €" },
+        { name: "Moyenne", price: "16,00 €" },
+        { name: "Grande", price: "21,00 €" },
+      ],
+    },
+    {
+      name: "CHICKEN",
+      description: "Crème fraîche, fromage, poulet, champignons, oignons",
+      options: [
+        { name: "Petite", price: "9,50 €" },
+        { name: "Moyenne", price: "16,00 €" },
+        { name: "Grande", price: "21,00 €" },
+      ],
+    },
+    {
+      name: "ROYALE",
+      description: "Crème fraîche, fromage, saumon, tomates, œuf",
+      options: [
+        { name: "Petite", price: "9,50 €" },
+        { name: "Moyenne", price: "16,00 €" },
+        { name: "Grande", price: "21,00 €" },
+      ],
+    },
+    {
+      name: "TARTIFLETTE",
+      description:
+        "Crème fraîche, fromage, lardons, reblochon, pommes de terre",
+      options: [
+        { name: "Petite", price: "9,50 €" },
+        { name: "Moyenne", price: "16,00 €" },
+        { name: "Grande", price: "21,00 €" },
+      ],
+    },
+    {
+      name: "CAMPAGNARDE",
+      description: "Crème fraîche, fromage, viande hachée, poulet, merguez",
+      options: [
+        { name: "Petite", price: "9,50 €" },
+        { name: "Moyenne", price: "16,00 €" },
+        { name: "Grande", price: "21,00 €" },
+      ],
+    },
+    {
+      name: "KEBAB",
+      description: "Crème fraîche, fromage, viande hachée, kebab, oignons",
+      options: [
+        { name: "Petite", price: "9,50 €" },
+        { name: "Moyenne", price: "16,00 €" },
+        { name: "Grande", price: "21,00 €" },
+      ],
+    },
+    {
+      name: "BOURSIN",
+      description: "Crème fraîche, fromage, viande hachée, boursin, oignons",
+      options: [
+        { name: "Petite", price: "9,50 €" },
+        { name: "Moyenne", price: "16,00 €" },
+        { name: "Grande", price: "21,00 €" },
+      ],
+    },
+    {
+      name: "CHEVRE MIEL",
+      description: "Crème fraîche, fromage, chèvre, miel",
+      options: [
+        { name: "Petite", price: "9,50 €" },
+        { name: "Moyenne", price: "16,00 €" },
+        { name: "Grande", price: "21,00 €" },
+      ],
+    },
   ],
-  "texmex": [
+
+  texmex: [
     {
-      "name": "Chicken Wings",
-      "description": "7 pièces",
-      "price": "8,50 €"
+      name: "Chicken Wings",
+      description: "7 pièces",
+      price: "8,50 €",
     },
     {
-      "name": "Nuggets",
-      "description": "8 pièces",
-      "price": "8,50 €"
+      name: "Nuggets",
+      description: "8 pièces",
+      price: "8,50 €",
     },
     {
-      "name": "Tenders",
-      "description": "6 pièces",
-      "price": "8,50 €"
+      name: "Tenders",
+      description: "6 pièces",
+      price: "8,50 €",
     },
     {
-      "name": "Frites",
-      "options": [
+      name: "Frites",
+      options: [
         {
-          "name": "Petite",
-          "price": "2,00 €"
+          name: "Petite",
+          price: "2,00 €",
         },
         {
-          "name": "Grande",
-          "price": "3,00 €"
-        }
-      ]
+          name: "Grande",
+          price: "3,00 €",
+        },
+      ],
     },
     {
-      "name": "Frites Cheddar Bacon",
-      "options": [
+      name: "Frites Cheddar Bacon",
+      options: [
         {
-          "name": "Petite",
-          "price": "3,00 €"
+          name: "Petite",
+          price: "3,00 €",
         },
         {
-          "name": "Grande",
-          "price": "4,00 €"
-        }
-      ]
-    }
+          name: "Grande",
+          price: "4,00 €",
+        },
+      ],
+    },
   ],
-  "paninis": [
+  paninis: [
     {
-      "name": "Viande Hachée",
-      "price": "6,00 €",
-      "specialNote": "+ 1 Boisson 33cl"
+      name: "Viande Hachée",
+      price: "6,00 €",
+      specialNote: "+ 1 Boisson 33cl",
     },
     {
-      "name": "3 Fromages",
-      "price": "6,00 €",
-      "specialNote": "+ 1 Boisson 33cl"
+      name: "3 Fromages",
+      price: "6,00 €",
+      specialNote: "+ 1 Boisson 33cl",
     },
     {
-      "name": "Poulet",
-      "price": "6,00 €",
-      "specialNote": "+ 1 Boisson 33cl"
+      name: "Poulet",
+      price: "6,00 €",
+      specialNote: "+ 1 Boisson 33cl",
     },
     {
-      "name": "Jambon",
-      "price": "6,00 €",
-      "specialNote": "+ 1 Boisson 33cl"
+      name: "Jambon",
+      price: "6,00 €",
+      specialNote: "+ 1 Boisson 33cl",
     },
     {
-      "name": "Merguez",
-      "price": "6,00 €",
-      "specialNote": "+ 1 Boisson 33cl"
+      name: "Merguez",
+      price: "6,00 €",
+      specialNote: "+ 1 Boisson 33cl",
     },
     {
-      "name": "Thon",
-      "price": "6,00 €",
-      "specialNote": "+ 1 Boisson 33cl"
-    }
+      name: "Thon",
+      price: "6,00 €",
+      specialNote: "+ 1 Boisson 33cl",
+    },
   ],
-  "zappwichs": [
+  zappwichs: [
     {
-      "name": "Viande Hachée",
-      "description": "Sauce tomate ou crème fraîche, fromage",
-      "price": "7,00 €"
+      name: "Viande Hachée",
+      description: "Sauce tomate ou crème fraîche, fromage",
+      price: "7,00 €",
     },
     {
-      "name": "Poulet",
-      "description": "Sauce tomate ou crème fraîche, fromage",
-      "price": "7,00 €"
+      name: "Poulet",
+      description: "Sauce tomate ou crème fraîche, fromage",
+      price: "7,00 €",
     },
     {
-      "name": "Jambon",
-      "description": "Sauce tomate ou crème fraîche, fromage",
-      "price": "7,00 €"
+      name: "Jambon",
+      description: "Sauce tomate ou crème fraîche, fromage",
+      price: "7,00 €",
     },
     {
-      "name": "Merguez",
-      "description": "Sauce tomate ou crème fraîche, fromage",
-      "price": "7,00 €"
+      name: "Merguez",
+      description: "Sauce tomate ou crème fraîche, fromage",
+      price: "7,00 €",
     },
     {
-      "name": "Cannibale",
-      "description": "Viande hachée, merguez, poulet",
-      "price": "8,00 €"
-    }
+      name: "Cannibale",
+      description: "Viande hachée, merguez, poulet",
+      price: "8,00 €",
+    },
   ],
   "menu-midi": [
     {
-      "name": "Menu Midi 1",
-      "description": "1 Pizza petite + 1 boisson 33cl",
-      "price": "10,00 €"
+      name: "Menu Midi 1",
+      description: "1 Pizza petite + 1 boisson 33cl",
+      price: "10,00 €",
     },
     {
-      "name": "Menu Midi 2",
-      "description": "1 Pizza moyenne + 1 boisson 33cl",
-      "price": "13,00 €"
-    }
+      name: "Menu Midi 2",
+      description: "1 Pizza moyenne + 1 boisson 33cl",
+      price: "13,00 €",
+    },
   ],
-  "desserts": [
+  desserts: [
     {
-      "name": "Tarte au daim",
-      "price": "2,50 €"
+      name: "Tarte au daim",
+      price: "2,50 €",
     },
     {
-      "name": "Tiramisu",
-      "price": "2,50 €"
-    }
+      name: "Tiramisu",
+      price: "2,50 €",
+    },
   ],
-  "boissons": [
+  boissons: [
     {
-      "name": "Coca, Coca zéro, Coca Cherry 33cl",
-      "price": "1,50 €"
+      name: "Coca, Coca zéro, Coca Cherry 33cl",
+      price: "1,50 €",
     },
     {
-      "name": "Fanta citron ou orange, Ice Tea 33cl",
-      "price": "1,50 €"
+      name: "Fanta citron ou orange, Ice Tea 33cl",
+      price: "1,50 €",
     },
     {
-      "name": "Sprite, Tropico 33cl",
-      "price": "1,50 €"
+      name: "Sprite, Tropico 33cl",
+      price: "1,50 €",
     },
     {
-      "name": "7 Up Mojito, Perrier 33cl",
-      "price": "1,50 €"
+      name: "7 Up Mojito, Perrier 33cl",
+      price: "1,50 €",
     },
     {
-      "name": "Oasis tropical 33cl",
-      "price": "1,50 €"
+      name: "Oasis tropical 33cl",
+      price: "1,50 €",
     },
     {
-      "name": "Maxi Coca, Fanta, Sprite 1,5L",
-      "price": "3,00 €"
+      name: "Maxi Coca, Fanta, Sprite 1,5L",
+      price: "3,00 €",
     },
     {
-      "name": "Maxi Oasis 2L",
-      "price": "3,50 €"
-    }
+      name: "Maxi Oasis 2L",
+      price: "3,50 €",
+    },
   ],
-  "sandwichs": [
+  sandwichs: [
     {
-      "name": "Grec",
-      "description": "Viande grecque",
-      "price": "9,00 €"
+      name: "Grec",
+      description: "Viande grecque",
+      price: "9,00 €",
     },
     {
-      "name": "Chicken",
-      "description": "Chicken curry, cheddar",
-      "price": "9,00 €"
+      name: "Chicken",
+      description: "Chicken curry, cheddar",
+      price: "9,00 €",
     },
     {
-      "name": "Escalope",
-      "description": "Escalope, crème fraîche, cheddar",
-      "price": "9,00 €"
+      name: "Escalope",
+      description: "Escalope, crème fraîche, cheddar",
+      price: "9,00 €",
     },
     {
-      "name": "Américain",
-      "description": "2 steaks, œuf, cheddar",
-      "price": "9,50 €"
+      name: "Américain",
+      description: "2 steaks, œuf, cheddar",
+      price: "9,50 €",
     },
     {
-      "name": "X2",
-      "description": "Chicken curry, steak, boursin, cheddar",
-      "price": "9,50 €"
+      name: "X2",
+      description: "Chicken curry, steak, boursin, cheddar",
+      price: "9,50 €",
     },
     {
-      "name": "Tenders Steak",
-      "description": "Tenders, steak, cheddar",
-      "price": "9,90 €"
-    }
+      name: "Tenders Steak",
+      description: "Tenders, steak, cheddar",
+      price: "9,90 €",
+    },
   ],
-  "tacos": [
+  tacos: [
     {
-      "name": "Tacos M",
-      "description": "1 viande, sauce fromagère et frites à l’intérieur",
-      "price": "7,00 €"
+      name: "Tacos M",
+      description: "1 viande, sauce fromagère et frites à l’intérieur",
+      price: "7,00 €",
     },
     {
-      "name": "Tacos L",
-      "description": "2 viandes, sauce fromagère et frites à l’intérieur",
-      "price": "8,00 €"
+      name: "Tacos L",
+      description: "2 viandes, sauce fromagère et frites à l’intérieur",
+      price: "8,00 €",
     },
     {
-      "name": "Tacos XL",
-      "description": "3 viandes, sauce fromagère et frites à l’intérieur",
-      "price": "9,00 €"
+      name: "Tacos XL",
+      description: "3 viandes, sauce fromagère et frites à l’intérieur",
+      price: "9,00 €",
     },
     {
-      "name": "Supplément fromage",
-      "price": "1,00 €"
+      name: "Supplément fromage",
+      price: "1,00 €",
     },
     {
-      "name": "Choix de viandes",
-      "description": "Cordon bleu, escalope, viande hachée, merguez, kebab, poulet, tenders, nuggets"
-    }
+      name: "Choix de viandes",
+      description:
+        "Cordon bleu, escalope, viande hachée, merguez, kebab, poulet, tenders, nuggets",
+    },
   ],
-  "burgers": [
+  burgers: [
     {
-      "name": "Cheese",
-      "description": "1 steak 45g, cheddar + frites + boisson 33cl",
-      "price": "6,50 €"
+      name: "Cheese",
+      description: "1 steak 45g, cheddar + frites + boisson 33cl",
+      price: "6,50 €",
     },
     {
-      "name": "Double Cheese",
-      "description": "2 steaks 45g, cheddar + frites + boisson 33cl",
-      "price": "7,50 €"
+      name: "Double Cheese",
+      description: "2 steaks 45g, cheddar + frites + boisson 33cl",
+      price: "7,50 €",
     },
     {
-      "name": "Chicken Burger",
-      "description": "Poulet pané, cheddar + frites + boisson 33cl",
-      "price": "7,50 €"
-    }
+      name: "Chicken Burger",
+      description: "Poulet pané, cheddar + frites + boisson 33cl",
+      price: "7,50 €",
+    },
   ],
-  "offres": [
+  offres: [
     {
-      "name": "Offre 1",
-      "description": "2 pizzas moyennes + 1 bouteille 1,5L",
-      "price": "24,00 €"
+      name: "Offre 1",
+      description: "2 pizzas moyennes + 1 bouteille 1,5L",
+      price: "24,00 €",
     },
     {
-      "name": "Offre 2",
-      "description": "3 pizzas petites + 1 bouteille 1,5L",
-      "price": "24,00 €"
+      name: "Offre 2",
+      description: "3 pizzas petites + 1 bouteille 1,5L",
+      price: "24,00 €",
     },
     {
-      "name": "Offre 3",
-      "description": "2 pizzas grandes + 1 bouteille 1,5L",
-      "price": "34,00 €"
-    }
+      name: "Offre 3",
+      description: "2 pizzas grandes + 1 bouteille 1,5L",
+      price: "34,00 €",
+    },
   ],
   "menu-enfant": [
     {
-      "name": "Menu Enfant",
-      "description": "4 nuggets ou 1 cheese + frites + Capri Sun + 1 Pom’Pote",
-      "price": "7,00 €"
-    }
-  ]
+      name: "Menu Enfant",
+      description: "4 nuggets ou 1 cheese + frites + Capri Sun + 1 Pom’Pote",
+      price: "7,00 €",
+    },
+  ],
 };
